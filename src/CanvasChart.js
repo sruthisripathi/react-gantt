8530b66c7ddae3f7cab09ad4fac36258754bb731
sruthi <sruthi@iotrl.io>
2019-01-29 01:38:32 +0530

    First commit

---
+++ src/CanvasChart.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { CanvasJSChart } from './ReactCanvas'; 
+
+class Chart extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataPoints: []
+    }
+  }
+
+  componentDidMount() {
+    let flags = [];
+    let data = this.props.data;
+    let l = data.length;
+    let output = {};
+    let count = 0;
+    for(let i = 0; i < l; i++) {
+      if(flags[data[i].xlabel]) continue;
+      count = count + 1;
+      flags[data[i].xlabel] = true;
+      output[data[i].xlabel] = count*10;
+    }
+
+    let points = this.props.data.map((p, i) => {
+      return {
+        x: output[p.xlabel],
+        y: p.y,
+        label: p.xlabel,
+        color: this.props.colors[p.clabel]      
+      }
+    });
+
+    this.setState({
+      dataPoints: points
+    });
+  }
+
+  renderLegend() {
+    let labels = [];
+    for(let legend in this.props.colors) {
+      labels.push(
+        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'space-between'}}>
+          <div style={{
+            width: '15px',
+            height: '15px',
+            backgroundColor: this.props.colors[legend]
+          }}></div> {legend}
+        </div>
+      )
+    }
+    return labels;
+  }
+
+  render() {
+		const options = {
+			animationEnabled: true,
+			title:{
+				text: this.props.title
+			},
+			axisY: {
+        interval: 1,
+        includeZero: false,
+        minimum: 0,
+        maximum: 12
+			},
+			data: [{
+        type: "rangeBar",
+				dataPoints: this.state.dataPoints
+			}]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				// onRef={ref => this.chart = ref}
+			/>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        {this.renderLegend()}        
+      </div>
+		</div>
+		);
+	}
+}
+
+export default Chart;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CanvasChart from './CanvasChart';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const data = [
+  { xlabel: "label 4", y: [2.1, 2.25], clabel: "label 4" },
+  { xlabel: "label 4", y: [3, 3.5], clabel: "label 6" },
+  { xlabel: "label 4", y: [4, 4.5], clabel: "label 6" },
+  { xlabel: "label 4", y: [3.15, 3.3], clabel: "label 4" },
+  { xlabel: "label 4", y: [4, 4.15], clabel: "label 4" },
+  { xlabel: "label 4", y: [4.95, 5], clabel: "label 4" },
+  { xlabel: "label 4", y: [10, 10.2], clabel: "label 4" },
+  { xlabel: "label 4", y: [11, 11.25], clabel: "label 4" },
+  { xlabel: "label 3", y: [3, 3.5], clabel: "label 6" },
+  { xlabel: "label 3", y: [4, 4.5], clabel: "label 6" },
+  { xlabel: "label 3", y: [9.75, 11.25], clabel: "label 3" },
+  { xlabel: "label 2", y: [0, 0.9], clabel: "label 2" },
+  { xlabel: "label 2", y: [3, 3.5], clabel: "label 6" },
+  { xlabel: "label 2", y: [4, 4.5], clabel: "label 6" },
+  { xlabel: "label 2", y: [4.7, 7.2], clabel: "lebel 2" },
+  { xlabel: "label 1", y: [3, 3.5], clabel: "label 6" },
+  { xlabel: "label 1", y: [0.9, 3.1], clabel: "label 1" },
+  { xlabel: "label 1", y: [4, 4.5], clabel: "label 6" },
+  { xlabel: "label 1", y: [11, 11.2], clabel: "label 5" },
+  { xlabel: "label 1", y: [1.3, 1.35], clabel: "label 5" },
+  { xlabel: "label 1", y: [4, 4.05], clabel: "label 5" }
+]
+
+const colors = {
+  'label 1': '#3CB371',
+  'label 2': '#20B2AA',
+  'label 3': '#4169E1',
+  'label 4': '#FFC0CB',
+  'label 5': '#E6E6FA',
+  'label 6': '#FF8080'
+}
+ReactDOM.render(<CanvasChart data={data} colors={colors} title={"Test chart"} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
